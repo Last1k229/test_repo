e1470dd8de4986d7e941ba326bdf854c0903e903
Last1k229 <oh.shit.im.sorr9@gmail.com>
2025-10-31 13:04:57 +0100

    some changes

--- learn.cpp
+++ learn.cpp
@@ -11,13 +11,14 @@ struct worker
 };
 
 int main(){
-    worker id233{"John", 21},
+    worker
+        id233{"John", 21},
         id254{"David", 27},
         id792{"Lusy", 32};
     id233.addnew(&id254);
     id254.addnew(&id792);
     for(worker* cursor=&id233; cursor; cursor=cursor->next){
-        std::cout << cursor->name<<" "<<cursor->age << "\n";
+        std::cout << cursor->name <<" "<< "age" << cursor->age << "\n";
     }
 
 }
