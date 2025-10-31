f0eea519d14e9a6fa188be4b5283f2f667790198
Last1k229 <oh.shit.im.sorr9@gmail.com>
2025-10-31 12:55:57 +0100

    Cpp file for learning code

---
+++ learn.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+struct worker
+{
+    char name[256];
+    int age{};
+    worker * next{};
+    void addnew(worker* new_worker){
+        new_worker->next=next;
+        next=new_worker;
+    }
+};
+
+int main(){
+    worker id233{"John", 21},
+        id254{"David", 27},
+        id792{"Lusy", 32};
+    id233.addnew(&id254);
+    id254.addnew(&id792);
+    for(worker* cursor=&id233; cursor; cursor=cursor->next){
+        std::cout << cursor->name<<" "<<cursor->age << "\n";
+    }
+
+}
