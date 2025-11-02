7ecc3bfdc742bb2be0296209b9ee32330c1a22bf
Last1k229 <oh.shit.im.sorr9@gmail.com>
2025-11-02 13:44:52 +0100

    throw, try, catch

--- learn.cpp
+++ learn.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
-struct Element {
-    Element (short num):operating_number{num}{}
-    Element* next{};
-    Element* before{};
-    void insert_after(Element* new_element) { 
-        new_element->next = next; 
-        next = new_element;
+#include <stdexcept>
+struct Ground{
+    void forget(int x){
+        if(x==0xFACE){
+            throw std::runtime_error{"Error"};
+        }
+        std::cout << "Forgot 0x"<< std::hex<<x <<"\n";
     }
-    void insert_before(Element* before_element){
-        before=before_element->before;
-        before_element=before;
-    } 
-    short operating_number;
 };
 int main(){
-    Element a{1}, b{2}, c{3};
-    a.insert_before(&b);
-    b.insert_before(&c);
-    for(auto cursor=&a; cursor; cursor=cursor->before){
-        std::cout << cursor->operating_number;
+    Ground ground;
+    try{
+        ground.forget(0xC0DE);
+        ground.forget(0xFACE);
+        ground.forget(0xC0FFEE);
+    }
+    catch (const std::runtime_error& e){
+        std::cout << "Cought and exception with message: "<< e.what();
     }
 
 }
